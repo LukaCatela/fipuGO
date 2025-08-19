41a17748e3db6699b8ecc57961e88d50b62350de
Luka Catela <luca.catela8@gmail.com>
2025-08-19 11:58:34 +0200

    Mapa Pula napravljena
    Napravia sam nacrt mape pule moran jos dovrsiti stranicu

--- fipuGo/src/components/Mapa_pu.vue
+++ fipuGo/src/components/Mapa_pu.vue
@@ -1,3 +1,131 @@
 <template>
-    Pu
+    <div>
+        <div id="map" class="h-200 w-300"></div>
+
+        <!--Kontrole layera-->
+        <div class="mt-2 space-x-2">
+            <button @click="sakrijLayer('bus-stops-layer')" class="px-4 py-2 bg-amber-100 text-black font-semibold rounded-lg shadow-mds hover:bg-amber-300">Stanice</button>
+            <button @click="sakrijLayer('bus-route-layer')"   class="px-4 py-2 bg-amber-100 text-black font-semibold rounded-lg shadow-mds hover:bg-amber-300">Linije</button>
+        </div>
+
+    </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+let map // globalna varijabla dostupna i unutar toggleLayer
+
+function sakrijLayer(layerId) {
+  if (!map.getLayer(layerId)) return // ako sloj ne postoji, prekini
+
+  const visibility = map.getLayoutProperty(layerId, 'visibility')
+
+  if (visibility === 'none' || !visibility) {
+    map.setLayoutProperty(layerId, 'visibility', 'visible')
+  } else {
+    map.setLayoutProperty(layerId, 'visibility', 'none')
+  }
+};
+
+
+onMounted(async ()=>{
+// setupiranje mape
+
+    map = new maplibregl.Map({
+        container: 'map',
+        style: 'https://tiles.openfreemap.org/styles/liberty', // openstreetmap
+        center: [13.848, 44.867], // koordinate centra mape
+        zoom: 13,
+        maxZoom: 20,
+        hash: true,
+        maxBounds: [
+        [13.75, 44.80], // jugozapadni kut (minLng, minLat)
+        [13.95, 44.95]  // sjeveroistočni kut (maxLng, maxLat)
+    ],
+    });
+
+    map.addControl(new maplibregl.NavigationControl(), 'top-right');
+
+    map.on('load', async() => {
+        const responseStanice = await fetch('/data/stanice_Pula.json'); //dodat putanju
+        const jsonDataStanice = await responseStanice.json();
+
+        const resopnseLinija = await fetch('/data/linije_Pula.json');
+        const jsonDataLinije = await resopnseLinija.json();
+
+
+        map.addSource('stanice_Pula', {type: 'geojson', data: jsonDataStanice});
+        map.addSource('linije_Pula', {type: 'geojson', data: jsonDataLinije});
+
+        // Stanice
+        map.addLayer({
+        id: 'bus-stops-layer',
+        type: 'circle',
+        source: 'stanice_Pula',
+        paint: {
+            'circle-radius': 3,
+            'circle-color': '#007bff',
+        },
+        layout: { visibility: 'visible' }, // početno vidljiv
+        })
+
+        // Linije
+        map.addLayer({
+            id: 'bus-route-layer',
+            type: 'line',
+            source: 'linije_Pula',
+            layout: {
+                'line-join': 'round',
+                'line-cap': 'round',
+                visibility: 'visible', // početno vidljiv
+            },
+            paint: {
+                'line-color': [
+                'match',
+                ['get', 'name'],
+                'Linija 1 - A.kolodvor-Stoja', '#ff4000',
+                'Linija 2 - Veruda - Verudela', '#007bff',
+                '#999999', // default siva
+                ],
+                'line-width': 2,
+            },
+        });
+
+        map.on('click', 'bus-stops-layer', (e) => { 
+            if (e.features.length > 0) { 
+                const feature = e.features[0]; 
+                const coordinates = feature.geometry.coordinates.slice(); 
+                const stationName = feature.properties.Name || 'Naziv nije dostupan'; 
+                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) { 
+                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360; } 
+                    new maplibregl.Popup() 
+                    .setLngLat(coordinates) 
+                    .setHTML(stationName) 
+                    .addTo(map); 
+            } 
+        });  
+
+        map.on('click', 'bus-route-layer', (e)=> {
+            if(e.features.length > 0){
+                const feature = e.features[0];
+                const routeName = feature.properties.name;
+
+                const coordinateMouse = e.lngLat;
+                new maplibregl.Popup()
+                .setLngLat(coordinateMouse)
+                .setHTML(routeName)
+                .addTo(map)
+            }
+        })
+
+        map.on('mouseenter', 'bus-stops-layer', () => { map.getCanvas().style.cursor = 'pointer'; }); 
+        map.on('mouseleave', 'bus-stops-layer', () => { map.getCanvas().style.cursor = ''; });
+        map.on('mouseenter', 'bus-route-layer', () => { map.getCanvas().style.cursor = 'pointer'; }); 
+        map.on('mouseleave', 'bus-route-layer', () => { map.getCanvas().style.cursor = ''; });
+    });
+
+});
+</script>
