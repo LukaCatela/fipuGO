f891e351801d7b9d8a76ef3ab4d038b6490660f2
Luka Catela <luca.catela8@gmail.com>
2025-08-26 15:38:57 +0200

    update linije javni prijevoz

--- fipuGo/src/views/JavniPrijevoz.vue
+++ fipuGo/src/views/JavniPrijevoz.vue
@@ -31,7 +31,26 @@ const currentComponent = computed(() => {
         <component :is="currentComponent" />
       </div>
       <div class="flex-1 p-4 m-3 bg-blue-50 rounded-xl shadow-md">
-        <p>Napisi nesto</p>
+        <div v-if="odabranGrad==='Pula'">
+          GRADSKE LINIJE / LINEE URBANE / URBAN LINES - ZONA 1
+          <p><b>1</b> Bus kolodvor → Stoja → Bus kolodvor</p>
+          <p><b>2a</b> Bus kolodvor → Veruda → Vidikovac → Šijana</p>
+          <p><b>3a</b> Šijana → Vidikovac → Verudela → Bus kolodvor</p>
+          <p><b>4</b> Veli Vrh → Stoja → Valkane → Veruda → Veli Vrh</p>
+          <p><b>5a</b> Giardini → Štinjan → Giardini</p>
+          <p><b>6</b> Jadreški → Kaštanjer → Veruda → Stoja → Kaštanjer → Jadreški</p>
+          <p><b>8</b> Bus kolodvor → Monte Magno → Valdebek → Bus kolodvor</p>
+          <p><b>9</b> Bus kolodvor → Monte Serpo → Valmade → Bus kolodvor</p>
+          <p><b>71</b> Centar → Šijana → Gregovica → Giardini → Stoja →Valkane
+          → Veruda → Verudela → Vidikovac → Centar</p>
+          <p><b>72</b> Centar → Partizanski put → Veli Vrh → Štinjan → Veli Vrh → Centar</p>
+          <p><b>73</b> Bus kolodvor → Monte Magno → Valdebek → Busoler
+          → Centar → Bus Kolodvor</p>
+        </div>
+        <div v-else="odabranGrad==='Zagreb'">
+          Nešto
+        </div>
+        
       </div>
     </div>
   </div>
